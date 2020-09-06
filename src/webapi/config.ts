458b71c7bc77e5692e27bfccf84964f91f3df6d0
lotsOfSnow <snowlotsof@gmail.com>
2020-09-06 20:36:14 +0200

    Use local profile and fix https url

--- src/webapi/config.ts
+++ src/webapi/config.ts
@@ -1,9 +1,9 @@
 import profiles from "./profiles.json";
 
 export const getApiUrl = (): string => {
-  return `${profiles.remote.apiUrl}`;
+  return `${profiles.local.apiUrl}`;
 };
 
 export const getImagesUrl = (): string => {
-  return `${profiles.remote.imagesUrl}`;
+  return `${profiles.local.imagesUrl}`;
 };
